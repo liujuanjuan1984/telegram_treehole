b7334aa886a7fb656804cb04f898352bd500526f
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2023-04-23 14:36:32 +0800

    [0.2.0] support for photo and post to tg channel as optional

--- do_it.py
+++ do_it.py
@@ -11,9 +11,12 @@ logging.basicConfig(
 class Config:
     RUM_SEED = "rum://seed?v=1&e=0&n=0&c=qOh_wTyuoKDoXxxxxxIaH5sa9TCLPkHCnpnROM8"
     ETH_PVTKEY = "0x5ee77ca3...effaf"
-    FEED_URL_BASE = "https://example/posts/"
+    FEED_URL_BASE = "https://example.com"
     TG_BOT_TOKEN = "1234566767:mybotkey"  # bot token
     TG_BOT_NAME = "@MyBotName"
+    TG_CHANNEL_NAME = "@MyChannelName"
+    TG_CHANNEL_URL = "https://t.me/MyChannelName"
+    SendToChannel = True  # if True, send to channel
     Developer = "@MyTelegramName"
     Seconds = 5  # no spam; each 5 seconds can only post one treehole
     ContentTag = "#Treehole "
--- telegram_treehole_bot.py
+++ telegram_treehole_bot.py
@@ -8,7 +8,7 @@ from quorum_data_py import feed
 from quorum_mininode_py import MiniNode
 from telegram.ext import Filters, MessageHandler, Updater
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 logger = logging.getLogger(__name__)
 logger.info("Version %s", __version__)
 
@@ -27,31 +27,52 @@ class TelegramTreeholeBot:
     def handle_private_chat(self, update, context):
         """send message to rum group and telegram channel"""
         logger.info("handle_private_chat")
-
         message_id = update.message.message_id
         userid = update.message.from_user.id
-        _text = update.message.text
+        _text = update.message.text or update.message.caption or ""
+        text = self.config.ContentTag + _text
+        _photo = update.message.photo
+        if _photo:
+            image = bytes(_photo[-1].get_file().download_as_bytearray())
+            if self.config.SendToChannel:
+                resp = self.tg_bot.send_photo(
+                    chat_id=self.config.TG_CHANNEL_NAME,
+                    photo=image,
+                    caption=text + f"\nFrom {self.config.TG_BOT_NAME}",
+                )
+        else:
+            image = None
+            if len(_text) > 1000 or len(_text) < 10:
+                self.tg_bot.send_message(
+                    chat_id=userid,
+                    text="⚠️ Message length must be between 10 and 1000 characters.",
+                    reply_to_message_id=message_id,
+                )
+                return
+            if self.config.SendToChannel:
+                resp = self.tg_bot.send_message(
+                    chat_id=self.config.TG_CHANNEL_NAME,
+                    text=text + f"\nFrom {self.config.TG_BOT_NAME}",
+                )
 
-        if _text is None or len(_text) > 1000 or len(_text) < 10:
-            self.tg_bot.send_message(
-                chat_id=userid,
-                text="⚠️ Message length must be between 10 and 1000 characters.",
-                reply_to_message_id=message_id,
-            )
-            return
-
-        logger.info("sleep 5 seconds %s", self.timer)
+        logger.info("timer is: %s", self.timer)
         while datetime.datetime.now() < self.timer + datetime.timedelta(
             seconds=self.config.Seconds
         ):
             logger.info("sleep 1 second")
             time.sleep(1)
+        if image:
+            data = feed.new_post(content=text, images=[image])
+        else:
+            data = feed.new_post(content=text)
 
-        data = feed.new_post(self.config.ContentTag + _text)
         data["origin"] = {
             "type": "telegram",
             "name": self.config.TG_BOT_NAME,
         }
+        if self.config.SendToChannel:
+            data["origin"]["url"] = f"{self.config.TG_CHANNEL_URL}/{resp.message_id}"
+
         resp = self.rum.api.post_content(data)
         if "trx_id" not in resp:
             raise ValueError(
@@ -60,9 +81,12 @@ class TelegramTreeholeBot:
         self.timer = datetime.datetime.now()
         logger.info("timer %s", self.timer)
 
-        feed_url = f"{self.config.FEED_URL_BASE}{data['object']['id']}"
+        feed_url = f"{self.config.FEED_URL_BASE}/posts/{data['object']['id']}"
         logger.info("feed_url %s", feed_url)
-        reply = "📤 Success to blockchain of rum group"
+        if self.config.SendToChannel:
+            reply = f"📤 Success to telegram channel {self.config.TG_CHANNEL_NAME} and blockchain of rum group."
+        else:
+            reply = f"📤 Success to blockchain of rum group."
         reply_markup = {
             "inline_keyboard": [[{"text": "⚜️Click here to view", "url": feed_url}]]
         }
@@ -79,7 +103,9 @@ class TelegramTreeholeBot:
         """run forever"""
         self.dispatcher.add_handler(
             MessageHandler(
-                Filters.text & ~Filters.command & Filters.chat_type.private,
+                (Filters.text | Filters.photo)
+                & ~Filters.command
+                & Filters.chat_type.private,
                 self.handle_private_chat,
             )
         )
